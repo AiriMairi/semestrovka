093f20f76ccbdbddfffdd22552fbd50d7f37fd11
AiriMairi <airinalatipova@yandex.ru>
2022-12-24 01:16:12 +0300

    + postgres

--- school/settings.py
+++ school/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = 'school.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "test-db",
+        "USER": "postgres",
+        "PASSWORD": "17456",
+        "HOST": "localhost",
+        "PORT": 5432
     }
 }
 
