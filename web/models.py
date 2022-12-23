2d1cb1077563710308f22ffb639aa50e4d02ddbc
AiriMairi <airinalatipova@yandex.ru>
2022-12-24 02:08:48 +0300

    + validators

--- web/models.py
+++ web/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from taggit.managers import TaggableManager
-
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class User(AbstractUser):
     avatar = models.ImageField(upload_to='users_avatars', blank=True, null=True)
