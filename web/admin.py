72291daaa91a4bc5f87a592f7ff6c595f9efc6b9
AiriMairi <airinalatipova@yandex.ru>
2022-12-24 03:28:28 +0300

    =edit admin panel

--- web/admin.py
+++ web/admin.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ['username', 'first_name', 'last_name', 'email']
+    list_display = ['username', 'first_name', 'last_name', 'email', 'image']
     filter_horizontal = ['groups', 'user_permissions']
     fields = ['password', 'last_login', 'groups', ('username', 'email'), ('first_name', 'last_name'), 'image',
               ('is_active', 'is_staff')]
